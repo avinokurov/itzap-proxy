f10c702b8f17f3116cff9f7f4389ba9335c513b1
Aleksey Vinokurov <avinokurov@pinnacle21.net>
2018-09-20 20:12:15 -0400

    make first unit test working...

--- proxy-lib/src/test/java/net/proxy/lib/ObjectBuilderTest.java
+++ proxy-lib/src/test/java/net/proxy/lib/ObjectBuilderTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import java.io.File;
 
 import static net.proxy.lib.ProxyUtils.newVersionInfo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class ObjectBuilderTest {
@@ -13,12 +15,16 @@ public class ObjectBuilderTest {
     public void buildTest() {
         // load test lib
         String libPath = ObjectBuilderTest.class.getResource("/").getPath();
+        File libDir = new File(libPath + "../../../test-lib/target/1.0");
+
         ProxyCallerInterface caller = ObjectBuilder.builder()
                 .setClassName("net.proxy.lib.test.LibClass")
                 .setArtifact(DirArtifact.builder()
                         .withClazz(ObjectBuilderTest.class)
-                        .withVersionInfo(newVersionInfo(new File(libPath)))
+                        .withVersionInfo(newVersionInfo(libDir))
                         .build())
                 .build();
+        String version = caller.call("getLibVersion").asString();
+        assertThat(version, is("1.0"));
     }
 }
